51aa444a8bc52896ba1437be3c102f5901b74a33
BlackHat039 <54604819+BlackHat039@users.noreply.github.com>
2019-10-18 00:07:39 -0300

    Add files via upload

---
+++ STA.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+# Criador: STA
+# Versão: 1.0
+
+apt update
+apt upgrade
+echo "Atualização dos repositórios concluída"
+ apt install python
+apt install python2
+apt install python3
+apt install clang
+apt install git
+apt install wget
+apt install bash
+apt install nano
+apt install cmatrix
+apt install pip
+apt install pip2
+apt install php
+apt install metasploit
+pkg install metasploit
+pkg install python
+pkg install python2
+pkg install python3
+pkg install bash
+pkg install php
+pkg install clang
+pkg install cmatrix
+pkg install nano
+pkg install git
+pkg install wget
+pkg install pip
+pkg install pip2
+echo "Instalação concluid
